8c4f93f8b6038cdfaa845f0003f7417fb71d1911
James Johnson <James@Jamess-MacBook-Pro.local>
2016-11-27 20:32:54 -0700

    Author Archive update.

--- wp-content/themes/theloveradicals/author.php
+++ wp-content/themes/theloveradicals/author.php
@@ -104,19 +104,6 @@
 
 						<?php endwhile; ?>
 
-						<?php else: ?>
-
-							<!-- article -->
-							<article>
-								<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-							</article>
-							<!-- /article -->
-
-						<?php endif; ?>
-
-
-
-
 
 
 
