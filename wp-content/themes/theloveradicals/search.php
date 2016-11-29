3cbb9a94671666ec11792e1bf105145a289c00f9
James Johnson <James@Jamess-MacBook-Pro.local>
2016-11-28 17:30:19 -0700

    Added search template.

--- wp-content/themes/theloveradicals/search.php
+++ wp-content/themes/theloveradicals/search.php
@@ -1,19 +1,27 @@
 <?php get_header(); ?>
 
-	<main role="main">
+	<main role="main container-fluid">
 		<!-- section -->
-		<section>
+		<section class="col-md-8 col-md-offset-2 row">
+			<article class="story row">
+				<div class=" col-md-12 page">
 
-			<h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
 
-			<?php get_template_part('loop'); ?>
+					<div class="col-md-8 col-md-offset-2 row">
+						<div class="post_info">
+							<h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
+						</div>
+					</div>
 
-			<?php get_template_part('pagination'); ?>
 
+					<section class="col-md-8 col-md-offset-2 row">
+						<?php get_template_part('/inc/loop'); ?>
+					</section>
+
+					<?php get_template_part('/inc/pagination'); ?>
+				</div>
+			</article>
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
-
 <?php get_footer(); ?>
